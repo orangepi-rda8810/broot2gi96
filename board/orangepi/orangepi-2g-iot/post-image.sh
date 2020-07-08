8065b0106ee23d69a00e0feaf198de41304fa479
thinhx2 <nthinh1102@gmail.com>
2020-07-08 13:18:40 +0700

    2g i96

--- board/orangepi/orangepi-2g-iot/post-image.sh
+++ board/orangepi/orangepi-2g-iot/post-image.sh
@@ -4,6 +4,9 @@ BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
+cp ${BOARD_DIR}/modem.bin ${BINARIES_DIR}/modem.bin
+#cp ${BOARD_DIR}/config.txt ${BINARIES_DIR}/config.txt
+
 rm -rf "${GENIMAGE_TMP}"
 
 genimage                               \
--- board/orangepi/orangepi-zero/post-image.sh
+++ board/orangepi/orangepi-zero/post-image.sh
@@ -1,1 +1,14 @@
-../post-image.sh
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage                               \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
